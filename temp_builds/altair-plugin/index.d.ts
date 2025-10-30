ae80ef878a54e124006999a46c52c47f6880d39c
Sam Yevmenenko <sam.y@flash-payments.com>
2025-10-30 14:27:17 +0300

    Build output for altair-plugin

---
+++ temp_builds/altair-plugin/index.d.ts
@@ -0,0 +1,3 @@
+import { PluginConstructor } from '../../altair-core/build/plugin/base';
+export declare const registerPluginClass: (pluginClassName: string, pluginClass: PluginConstructor) => void;
+//# sourceMappingURL=index.d.ts.map
