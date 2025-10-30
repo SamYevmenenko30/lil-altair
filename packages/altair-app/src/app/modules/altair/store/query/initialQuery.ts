83c0075a01fcecbcfa075b47aa1f4bd73221c5d8
Sam Yevmenenko <sam.y@flash-payments.com>
2025-10-30 23:44:49 +0300

    Build output for altair-app

--- packages/altair-app/src/app/modules/altair/store/query/initialQuery.ts
+++
@@ -1,8 +0,0 @@
-const initialQuery = `
-# Welcome to Altair GraphQL Client.
-# You can send your request using CmdOrCtrl + Enter.
-
-# Enter your graphQL query here.
-`.trimStart();
-
-export { initialQuery };
