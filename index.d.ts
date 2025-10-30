ecba34cd8187ff2329328620dc26fc6e520c450f
Sam Yevmenenko <sam.y@flash-payments.com>
2025-10-30 15:20:13 +0300

    Build output for transactional

---
+++ index.d.ts
@@ -0,0 +1,3 @@
+import { Options } from '@react-email/components';
+import { WelcomeEmailProps } from './emails/Welcome';
+export declare const renderWelcomeEmail: (props: WelcomeEmailProps, options?: Options) => Promise<string>;
